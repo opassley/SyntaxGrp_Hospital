8672e631bd15b0e73bb1cfec18737a12aabb7f27
ONeil Passley <opassley@gmail.com>
2023-04-14 21:23:09 -0500

    Add project files.

---
+++ SyntaxMedical.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SyntaxMedical.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
