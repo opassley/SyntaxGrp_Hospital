7438431908928748592624814320315d49c6f34a
ONeil Passley <opassley@gmail.com>
2023-04-17 04:54:14 -0500

    Database UPdated

--- SyntaxMedical.web/Data/Migrations/20230417095023_ExtendedUserTable_HGatesEmployee.Designer.cs
+++ SyntaxMedical.web/Data/Migrations/20230417095023_ExtendedUserTable_HGatesEmployee.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SyntaxMedical.web.Data;
 
@@ -11,9 +12,10 @@ using SyntaxMedical.web.Data;
 namespace SyntaxMedical.web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230417095023_ExtendedUserTable_HGatesEmployee")]
+    partial class ExtendedUserTable_HGatesEmployee
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SyntaxMedical.web/Data/Migrations/20230417095023_ExtendedUserTable_HGatesEmployee.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyntaxMedical.web.Data.Migrations
+{
+    public partial class ExtendedUserTable_HGatesEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DateJoined",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StaffID",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateJoined",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "StaffID",
+                table: "AspNetUsers");
+        }
+    }
+}
