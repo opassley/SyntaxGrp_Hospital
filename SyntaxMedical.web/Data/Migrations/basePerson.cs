a39e6fa19cd008bd7cbce984b1d7fc1fda007b94
ONeil Passley <opassley@gmail.com>
2023-04-17 23:58:15 -0500

    create base class for all persons to inherit from with id, firstname, lastname

---
+++ SyntaxMedical.web/Data/Migrations/basePerson.cs
@@ -0,0 +1,9 @@
+﻿namespace SyntaxMedical.web.Data.Migrations
+{
+    public partial class basePerson
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+    }
+}
